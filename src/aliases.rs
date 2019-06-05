7ffe94bb98ce329f28dff683cacc9171d3bd89dc
dalesnail <cameronmills.work@gmail.com>
2019-06-05 17:32:53 -0400

    Incrimental improvement

--- src/aliases.rs
+++ src/aliases.rs
@@ -1,14 +1,10 @@
 use std::fs::OpenOptions;
 use std::io::Write;
-// use std::fs;
-// use std::fs::File;
-// use std::io::prelude::*;
 
 pub fn definealias(alias: String, filepath: String) {
     let alias = "-<<<>  ".to_owned() + &alias + ": " + &filepath + "\n";
     let mut aliasfile = OpenOptions::new()
         .read(true)
-        // .write(true)
         .append(true)
         .open("crowfile")
         .unwrap();
--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,15 @@ If not, see <https://www.gnu.org/licenses/>.")
 
     // Pulls the given argument, and converts it into a string, to feed to aliases.rs. Will need to
     // update this later to write that string to a plain text file.
+
     if let Some(setalias) = crow.value_of("setalias") {
         if let Some(alias) = crow.value_of("alias") {
             aliases::definealias(alias.to_string(), setalias.to_string());
         }
     }
+
+    if let Some(open) = crow.value_of("alias") {
+        println!("{}", open); 
+    }
+
 }
