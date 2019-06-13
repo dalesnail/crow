e0d3b6e1a9d855d394179d9a9842b5dddee67898
dalesnail <cameronmills.work@gmail.com>
2019-06-13 17:25:54 -0400

    Updates to openalias fn, getting this to open crowfile from anywhere on the PC

--- src/aliases.rs
+++ src/aliases.rs
@@ -2,7 +2,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::fs::OpenOptions;
 use std::io::Write;
-use regex::*;
+use std::process::Command;
+use std::path::PathBuf;
+use std::path::Path;
+extern crate directories;
+use directories::{BaseDirs, UserDirs, ProjectDirs};
 
 pub fn definealias(alias: String, filepath: String) {
     let alias = "-<<<>  ".to_owned() + &alias + ": " + &filepath + "\n";
@@ -17,16 +21,25 @@ pub fn definealias(alias: String, filepath: String) {
 
 pub fn openalias(alias: String) {
     let srchterm = "-<<<>  ".to_owned() + &alias + ": ";
-    let aliasfile = BufReader::new(File::open("crowfile").unwrap());
-        for line in aliasfile.lines() {
-            match line {
-                Ok(line) => if line.starts_with(&srchterm) {
-                    let path: Vec<_> = line.split(": ").collect();
-                    let file = &path[1];
-                    println!("{}", file);
-                },
+    if let Some(home) = BaseDirs::new() {
+        let mut path = PathBuf::from(home.config_dir());
+        path.push("crow");
+        path.push("crowfile");
+        let aliasfile = BufReader::new(File::open(path).unwrap());
+            for line in aliasfile.lines() {
+                match line { 
+                    Ok(line) => if line.starts_with(&srchterm) {
+                        let path: Vec<_> = line.split(": ").collect();
+                        let file = &path[1]; 
+                        Command::new("nvim")
+                            .arg(file)
+                            .status()
+                            .expect("Something went wrong.");
+                        println!("{}", file);
+                        println!("{:#?}", path);
+                    },
                 Err(e) => panic!("Error reading file: {}", e)
-            }
-        
+                }
         }
+    }
 }
