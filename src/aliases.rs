ebbdd88cc4b8381734cd28559da858fe81202feb
dalesnail <cameronmills.work@gmail.com>
2019-06-14 09:21:12 -0400

    Openalias updated to grab crowfile from config folder, rather than CWD

--- src/aliases.rs
+++ src/aliases.rs
@@ -10,33 +10,35 @@ use directories::{BaseDirs, UserDirs, ProjectDirs};
 
 pub fn definealias(alias: String, filepath: String) {
     let alias = "-<<<>  ".to_owned() + &alias + ": " + &filepath + "\n";
-    let mut aliasfile = OpenOptions::new()
-        .read(true)
-        .append(true)
-        .open("crowfile")
-        .unwrap();
-    aliasfile.write_all(alias.as_bytes())
-        .expect("Could not write to file");
+    if let Some(home) = ProjectDirs::from("com", "gnucrow", "crow") {
+        let mut path = PathBuf::from(home.config_dir());
+        path.push("crowfile");
+        let mut aliasfile = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(&path)
+            .unwrap();
+        aliasfile.write_all(alias.as_bytes())
+            .expect("Could not write to file");
+    }
 }
 
 pub fn openalias(alias: String) {
     let srchterm = "-<<<>  ".to_owned() + &alias + ": ";
-    if let Some(home) = BaseDirs::new() {
+    if let Some(home) = ProjectDirs::from("com", "gnucrow", "crow") {
         let mut path = PathBuf::from(home.config_dir());
-        path.push("crow");
         path.push("crowfile");
-        let aliasfile = BufReader::new(File::open(path).unwrap());
+        let aliasfile = BufReader::new(File::open(&path).unwrap());
             for line in aliasfile.lines() {
                 match line { 
                     Ok(line) => if line.starts_with(&srchterm) {
-                        let path: Vec<_> = line.split(": ").collect();
-                        let file = &path[1]; 
+                        let aliaspath: Vec<_> = line.split(": ").collect();
+                        let file = &aliaspath[1]; 
                         Command::new("nvim")
                             .arg(file)
                             .status()
                             .expect("Something went wrong.");
-                        println!("{}", file);
-                        println!("{:#?}", path);
+                        println!("Path: {:?}", &path);
                     },
                 Err(e) => panic!("Error reading file: {}", e)
                 }
